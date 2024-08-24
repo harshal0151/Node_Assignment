ca2de591f30ae7d84ad935645a1f61302a4a73cb
harshal0151 <patilharshal0151@gmail.com>
2024-08-24 20:21:22 +0530

    created npm, package

---
+++ Day3 Create npm/npm package 1/index.js
@@ -0,0 +1,18 @@
+export function binarySearch(sortedArray, target) {
+    let left = 0;
+    let right = sortedArray.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (sortedArray[mid] === target) {
+            return mid; // Target found
+        } else if (sortedArray[mid] < target) {
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+
+    return -1; // Target not found
+}
---
+++ Day3 Create npm/npm package 1/test.js
@@ -0,0 +1,6 @@
+import {binarySearch} from './index.js'
+
+let arr = [2,4,7,9,11,45,50];
+
+let searchedIdx = binarySearch(arr , 9);
+console.log(searchedIdx);
