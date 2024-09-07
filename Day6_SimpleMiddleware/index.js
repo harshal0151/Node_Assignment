7956b9d769230240fd98e4b39c404ee52bdd8a8b
harshal0151 <patilharshal0151@gmail.com>
2024-09-07 23:31:26 +0530

    day6

---
+++ Day6_SimpleMiddleware/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const logger = require("./middleware/logger");
+const app = express();
+
+const PORT = 8000
+
+
+app.use(logger)
+
+app.get("/", (req , res)=>{
+     res.send("hello from Ibrahim ")
+})
+
+
+app.listen(PORT , ()=>{
+    console.log("server running at ",PORT)
+})
